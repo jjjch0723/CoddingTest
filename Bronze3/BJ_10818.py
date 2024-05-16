5b6782a8c939021d434b7e26cbf7537037b15589
jjjch0723 <83578340+jjjch0723@users.noreply.github.com>
2024-05-16 20:39:25 +0900

    Add files via upload

---
+++ Bronze3/BJ_10818.py
@@ -0,0 +1,4 @@
+x = input()
+nums = list(map(int, input().split()))
+
+print(min(nums), max(nums))
---
+++ Bronze3/BJ_2884.py
@@ -0,0 +1,15 @@
+a = input()
+a = a.split()
+
+h = int(a[0])
+m = int(a[1])
+
+if m < 45 :
+    if h == 0:
+        h=23
+        m=m+60
+    else : 
+        h=h-1
+        m=m+60
+
+print(h, m-45)

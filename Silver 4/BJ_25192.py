9af02d9a1ef96b9bc6dd31261accfeb5d992261d
jjjch0723 <83578340+jjjch0723@users.noreply.github.com>
2024-06-02 16:46:03 +0900

    Create BJ_25192.py

---
+++ Silver 4/BJ_25192.py
@@ -0,0 +1,17 @@
+def gomgomi(n):
+    gomgom = set()
+    cnt = 0
+    for _ in range(n):
+        word = input()
+
+        if word != 'ENTER':
+            if word not in gomgom:
+                cnt += 1
+                gomgom.add(word)
+        elif word == 'ENTER':
+            gomgom.clear()
+
+    return cnt
+
+
+print(gomgomi(int(input())))

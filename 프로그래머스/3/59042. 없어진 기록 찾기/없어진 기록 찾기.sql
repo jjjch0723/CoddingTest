65f8af08d0809b525f61dcbfa916631cb3f748c4
jjjch0723 <83578340+jjjch0723@users.noreply.github.com>
2024-08-12 17:36:58 +0900

    Update 없어진 기록 찾기.sql

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,4 +1,7 @@
 -- 코드를 입력하세요
+# 조인 조건 : animal_id
+# 데이터가 유실된 테이블 : animal_ins
+# left혹은 right 조인 이용해서(left를 사용시 animal_out테이블을 from절에 사용) animal_ins에서 null 값 찾아내기
 SELECT ao.animal_id, ao.name
   from animal_ins as ai
  right join animal_outs as ao on ai.animal_id = ao.animal_id

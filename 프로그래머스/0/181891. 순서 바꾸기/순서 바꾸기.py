a67d5e3030784ba35c12e511b790c42b6ed11fe6
jjjch0723 <83578340+jjjch0723@users.noreply.github.com>
2024-11-08 23:16:52 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.py
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.py
@@ -1,7 +1,2 @@
 def solution(num_list, n):
-    answer=[]
-    for j in range(n, len(num_list)):
-        answer.append(num_list[j])
-    for i in range(0, n):
-        answer.append(num_list[i])
-    return answer
+    return num_list[n:] + num_list[:n]
